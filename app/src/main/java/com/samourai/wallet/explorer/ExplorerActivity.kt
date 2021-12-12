fa66c7f164d6ddf236f8d9eb49ae76a8c29eb7b3
Sarath <sarath@samourai.io>
2021-12-12 13:49:41 +0530

    WIP: Explorer activity

---
+++ app/src/main/java/com/samourai/wallet/explorer/ExplorerActivity.kt
@@ -0,0 +1,12 @@
+package com.samourai.wallet.explorer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.samourai.wallet.R
+
+class ExplorerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explorer)
+    }
+}
