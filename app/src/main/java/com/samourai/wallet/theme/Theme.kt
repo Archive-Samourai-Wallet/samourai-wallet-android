3bd901a5f71795c0847d204a84214d837257be9f
sarath <sarath@samourai.io>
2022-05-23 23:50:26 +0530

    Add: accompanist-insets

--- app/src/main/java/com/samourai/wallet/theme/Theme.kt
+++ app/src/main/java/com/samourai/wallet/theme/Theme.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = samouraiSlateGreyAccent,
