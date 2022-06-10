a9b713db0c32cc1709e2f64d5ae3880c3bfc1076
sarath <sarath@samourai.io>
2022-06-10 12:56:33 +0530

    [Add] Material fade-scale anim for compose views

--- app/src/main/java/com/samourai/wallet/tools/AnimatedWrapper.kt
+++ app/src/main/java/com/samourai/wallet/tools/AnimatedWrapper.kt
@@ -5,6 +5,8 @@ import androidx.compose.animation.core.FastOutLinearInEasing
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 private const val ProgressThreshold = 0.35f
 
@@ -14,20 +16,42 @@ private val Int.ForOutgoing: Int
 private val Int.ForIncoming: Int
     get() = this - this.ForOutgoing
 
+//Material Motion durations
+object MotionConstants {
+    const val motionDurationShort1 = 75
+    const val motionDurationShort2 = 150
+    const val motionDurationMedium1 = 200
+    const val motionDurationMedium2 = 250
+    const val motionDurationLong1 = 300
+    const val motionDurationLong2 = 350
+    val DefaultSlideDistance: Dp = 30.dp
+}
+
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun WrapToolsPageAnimation(visible: Boolean, content: @Composable() AnimatedVisibilityScope.() -> Unit) {
+fun WrapToolsPageAnimation(visible: Boolean,
+                           initialScale: Float = 0.92f,
+                           durationMillis: Int = MotionConstants.motionDurationLong1,
+                           content: @Composable() AnimatedVisibilityScope.() -> Unit) {
     AnimatedVisibility(
         visible = visible,
         enter = fadeIn(
             animationSpec = tween(
-                durationMillis = 350.ForIncoming,
-                delayMillis = 2.ForOutgoing,
+                durationMillis = durationMillis.ForIncoming,
+                delayMillis = durationMillis.ForOutgoing,
                 easing = LinearOutSlowInEasing
             )
+        ) + scaleIn(
+            animationSpec = tween(
+                durationMillis = durationMillis.ForIncoming,
+                delayMillis = durationMillis.ForOutgoing,
+                easing = LinearOutSlowInEasing
+            ),
+            initialScale = initialScale
         ),
         exit = fadeOut(
             animationSpec = tween(
-                durationMillis = 350.ForOutgoing,
+                durationMillis = durationMillis.ForOutgoing,
                 delayMillis = 0,
                 easing = FastOutLinearInEasing
             )
