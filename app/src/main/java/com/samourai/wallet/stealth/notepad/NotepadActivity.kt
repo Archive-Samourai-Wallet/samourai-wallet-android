5e8817f3be5fd15eadf71751377935a36d24652e
miguel <miguelm9@protonmail.com>
2022-10-18 12:02:56 +0200

    Ask for stealth code when exiting notepad

--- app/src/main/java/com/samourai/wallet/stealth/notepad/NotepadActivity.kt
+++ app/src/main/java/com/samourai/wallet/stealth/notepad/NotepadActivity.kt
@@ -4,30 +4,31 @@ import android.app.Activity
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
-import android.preference.PreferenceManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -40,17 +41,18 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.edit
 import androidx.core.graphics.ColorUtils
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.samourai.wallet.R
 import com.samourai.wallet.stealth.StealthModeController
 import com.samourai.wallet.stealth.qrscannerapp.Purple40
 import com.samourai.wallet.stealth.qrscannerapp.PurpleGrey40
 import com.samourai.wallet.tools.WrapToolsPageAnimation
-import com.samourai.wallet.util.PrefsUtil
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -123,7 +125,6 @@ fun NotesScreen() {
                         modifier = Modifier.pointerInput(Unit){
                             detectTapGestures(
                                 onDoubleTap = {
-                                    StealthModeController.enableStealth(StealthModeController.StealthApp.SAMOURAI, context)
                                     pinEntryValue = ""
                                     pinEntryDialog = true
                                     scope.launch {
@@ -231,6 +232,64 @@ fun NotesScreen() {
                 }
             }
         }
+    }
+    if (pinEntryDialog) {
+        AlertDialog(
+            shape = RoundedCornerShape(8.dp),
+            tonalElevation = 12.dp,
+            onDismissRequest = {
+                pinEntryDialog = false
+            },
+            title = {
+                Text(text = "Enter Stealth Code", fontSize = 14.sp)
+            },
+            modifier = Modifier.padding(horizontal = 8.dp, vertical = 10.dp),
+            text = {
+                TextField(
+                    value = pinEntryValue,
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = Color.Transparent,
+                        textColor = Color.White, cursorColor = Color.White,
+                        focusedIndicatorColor = Color.White
+                    ),
+                    keyboardOptions = KeyboardOptions(
+                        autoCorrect = false,
+                        keyboardType = KeyboardType.Decimal,
+                    ),
+                    modifier = Modifier.focusRequester(focusRequester),
+                    onValueChange = {
+                        if (!it.contains("*") && !it.contains("-") && !it.contains(",") && !it.contains(".") && !it.contains(" ")) {
+                            pinEntryValue = it
+                        }
+                    },
+                )
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    pinEntryDialog = false
+                }) {
+                    Text(text = "Cancel")
+                }
+            },
+            confirmButton = {
+                TextButton(onClick = {
+                    pinEntryDialog = false
+                    if (pinEntryValue.isNotEmpty() && StealthModeController.isPinMatched(context, pinEntryValue)) {
+                        MaterialAlertDialogBuilder(context)
+                            .setTitle(R.string.app_name)
+                            .setMessage(R.string.do_you_want_to_disable_stealth_mode)
+                            .setPositiveButton(R.string.ok) { dialog, _ ->
+                                dialog.dismiss()
+                                StealthModeController.enableStealth(StealthModeController.StealthApp.SAMOURAI, context)
+                            }.setNegativeButton(R.string.cancel) { dialog, _ ->
+                                dialog.dismiss()
+                            }.show()
+                    }
+                }) {
+                    Text(text = "Ok")
+                }
+            }
+        )
     }
 }
 
