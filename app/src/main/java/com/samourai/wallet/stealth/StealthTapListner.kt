3aacba09da14224270579568c2086e4eb782c5ae
sarath <sarath@samourai.io>
2022-10-24 13:05:25 +0530

    improved stealth listener

--- app/src/main/java/com/samourai/wallet/stealth/StealthTapListner.kt
+++ app/src/main/java/com/samourai/wallet/stealth/StealthTapListner.kt
@@ -1,23 +1,26 @@
 package com.samourai.wallet.stealth
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.input.pointer.pointerInput
 
 
 fun Modifier.stealthTapListener(
     onTapCallBack: () -> Unit = {},
-    click: () -> Unit = {},
+    click: ( () -> Unit)? = null,
     taps: Int = 5,
 ) = composed {
     var lastTap by remember { mutableStateOf(System.currentTimeMillis()) }
     var numberOfTaps by remember { mutableStateOf(0) }
-    this.clickable {
-        click.invoke()
+
+    fun click(){
+        click?.invoke()
         if (System.currentTimeMillis().minus(lastTap) < 2500) {
             numberOfTaps += 1
             lastTap = System.currentTimeMillis()
@@ -30,6 +33,18 @@ fun Modifier.stealthTapListener(
         if (numberOfTaps == taps) {
             onTapCallBack.invoke()
             numberOfTaps = 0
+        }
+    }
+
+    if(click == null){
+        this.pointerInput(Unit){
+            detectTapGestures {
+                click()
+            }
+        }
+    }else{
+        this.clickable {
+            click()
         }
     }
 }
