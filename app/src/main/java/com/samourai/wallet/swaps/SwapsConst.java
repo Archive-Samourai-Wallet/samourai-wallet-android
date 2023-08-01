8012bbbff3d6c376b77e0c0f9b59755aac09f29b
samouraidev <dev@samouraiwallet.com>
2023-08-01 16:14:21 +0100

    SwapsConst: use Whirlpool bad bank for offsets

--- app/src/main/java/com/samourai/wallet/swaps/SwapsConst.java
+++ app/src/main/java/com/samourai/wallet/swaps/SwapsConst.java
@@ -3,7 +3,6 @@ package com.samourai.wallet.swaps;
 import static com.samourai.wallet.whirlpool.WhirlpoolConst.WHIRLPOOL_BADBANK_ACCOUNT;
 
 public class SwapsConst {
-//    public final static int SWAPS_REFUND_ACCOUNT = WHIRLPOOL_BADBANK_ACCOUNT - 1;
-    public final static int SWAPS_MAIN_ACCOUNT = Integer.MAX_VALUE - 4;
-    public final static int SWAPS_REFUND_ACCOUNT = Integer.MAX_VALUE - 5;
+    public final static int SWAPS_MAIN_ACCOUNT = WHIRLPOOL_BADBANK_ACCOUNT - 1;
+    public final static int SWAPS_REFUND_ACCOUNT = WHIRLPOOL_BADBANK_ACCOUNT - 2;
 }
