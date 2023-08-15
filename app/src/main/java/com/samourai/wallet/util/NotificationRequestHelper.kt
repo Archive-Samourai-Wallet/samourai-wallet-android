a02d9900eb414d96da7144e11d337b74c0caf4c1
brz <brz@samourai.is>
2023-08-15 23:26:34 +0200

    Tor status / Whirlpool status not in Android notification bar

---
+++ app/src/main/java/com/samourai/wallet/util/NotificationRequestHelper.kt
@@ -0,0 +1,38 @@
+package com.samourai.wallet.util
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+
+fun askNotificationPermission(activity: ComponentActivity) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        val permission =
+            ContextCompat.checkSelfPermission(activity, Manifest.permission.POST_NOTIFICATIONS)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            if (activity.shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                Log.d(TAG, "Post notifications : permission denied by user")
+            } else {
+
+                val requestPermissionLauncher = activity.registerForActivityResult(
+                    ActivityResultContracts.RequestPermission()
+                ) { isGranted: Boolean ->
+                    if (isGranted) {
+                        Log.i(TAG, "User granted permission to post notifications")
+                    } else {
+                        Log.i(TAG, "User denied permission to post notifications")
+                    }
+                }
+
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        } else {
+            Log.d(TAG, "Post notifications : permission granted by user")
+        }
+    }
+}
+
+private const val TAG = "NotificationRequest"
