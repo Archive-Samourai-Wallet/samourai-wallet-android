ee31871fe455aa1eb3ddb1ff8e39318f8b71deaa
samouraidev <dev@samouraiwallet.com>
2024-01-26 17:06:51 +0000

    refactor account indexes

--- app/src/main/java/com/samourai/wallet/swaps/SwapsConst.java
+++ app/src/main/java/com/samourai/wallet/swaps/SwapsConst.java
@@ -1,10 +1,10 @@
 package com.samourai.wallet.swaps;
 
-import static com.samourai.wallet.whirlpool.WhirlpoolConst.WHIRLPOOL_BADBANK_ACCOUNT;
+import com.samourai.whirlpool.client.wallet.beans.SamouraiAccountIndex;
 
 public class SwapsConst {
-    public final static int SWAPS_MAIN_ACCOUNT = WHIRLPOOL_BADBANK_ACCOUNT - 1;
-    public final static int SWAPS_REFUND_ACCOUNT = WHIRLPOOL_BADBANK_ACCOUNT - 2;
+    public final static int SWAPS_MAIN_ACCOUNT = SamouraiAccountIndex.SWAPS_DEPOSIT;
+    public final static int SWAPS_REFUND_ACCOUNT = SamouraiAccountIndex.SWAPS_REFUNDS;
 
-    public final static int SWAPS_ASB_MAIN_ACCOUNT = WHIRLPOOL_BADBANK_ACCOUNT - 3;
+    public final static int SWAPS_ASB_MAIN_ACCOUNT = SamouraiAccountIndex.SWAPS_ASB;
 }
