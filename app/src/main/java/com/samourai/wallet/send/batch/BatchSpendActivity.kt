ca938cc020d320273273a9b4ad6438224b880bec
Wandering King 072 <miguel@samourai.is>
2024-04-23 09:17:58 +0200

    Dont allow user to enter commas when inputting the sat value

--- app/src/main/java/com/samourai/wallet/send/batch/BatchSpendActivity.kt
+++ app/src/main/java/com/samourai/wallet/send/batch/BatchSpendActivity.kt
@@ -49,10 +49,10 @@ import com.samourai.wallet.send.FeeUtil
 import com.samourai.wallet.send.MyTransactionOutPoint
 import com.samourai.wallet.send.RBFSpend
 import com.samourai.wallet.send.SendActivity
+import com.samourai.wallet.send.SendActivity.isPSBT
 import com.samourai.wallet.send.SendFactory
 import com.samourai.wallet.send.SendParams
 import com.samourai.wallet.send.UTXO
-import com.samourai.wallet.send.SendActivity.isPSBT
 import com.samourai.wallet.send.UTXO.UTXOComparator
 import com.samourai.wallet.send.batch.InputBatchSpendHelper.loadInputBatchSpend
 import com.samourai.wallet.send.cahoots.ManualCahootsActivity
@@ -85,6 +85,7 @@ import java.lang.String.format
 import java.math.BigInteger
 import java.net.URLDecoder
 import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 import java.text.NumberFormat
 import java.text.ParseException
 import java.util.Collections
@@ -381,7 +382,14 @@ class BatchSpendActivity : SamouraiActivity() {
                     btcEditText.addTextChangedListener(btcWatcher)
                     return
                 }
+
                 val spaceRemoved = editable.toString().replace(" ", "")
+                    .replace(
+                        java.lang.String.valueOf(DecimalFormatSymbols(Locale.getDefault()).getGroupingSeparator()),
+                        ""
+                    )
+
+
                 val sats = spaceRemoved.toDouble()
                 val btc: Double = getBtcValue(sats)
                 val formatted: String? = formattedSatValue(sats)
